29d65cc73a459ee36058345dd4886263e74b1914
Ike <ikescode@gmail.com>
2020-06-04 23:19:24 -0500

    add weather data to table

--- src/components/mood-list.js
+++ src/components/mood-list.js
@@ -18,22 +18,31 @@ class MoodList extends Component {
                 </div>
                 <div className="card-body">
                   <h4 className="card-title text-center text-muted"><strong>{mood}</strong></h4>
-                  <table class="table table-sm">
+                  <table class="table table-sm borderless">
+                    <thead>
+                      <tr>
+                        <th scope="col">Temp</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Wind</th>
+                      </tr>
+                    </thead>
                     <tbody>
                       <tr>
-                        <td>{toFahrenheit(weather.main.temp)}</td>
+                        <td>Current: <strong>{toFahrenheit(weather.main.temp)}°</strong></td>
                         <td>{capitalize(weather.weather[0].description)}</td>
-                        <td>@mdo</td>
+                        <td><strong>{weather.wind.speed}</strong> mph</td>
                       </tr>
                       <tr>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Feels Like: <strong>{toFahrenheit(weather.main.feels_like)}°</strong></td>
+                        <td></td>
+                        <td><strong>{weather.wind.deg}</strong> deg</td>
                       </tr>
                       <tr>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
+                        <td>High: <strong>{toFahrenheit(weather.main.temp_max)}°</strong></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>Low: <strong>{toFahrenheit(weather.main.temp_min)}°</strong></td>
                       </tr>
                     </tbody>
                   </table>
